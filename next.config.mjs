5a8ca0540a2261eff3c668414b4f477030aa8f48
Sasha Diachenko <sashkhen056@gmail.com>
2024-04-09 01:24:39 +0200

    ~

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/pijanyzuk",
-  output: "export", // <=== enables static exports
+  // basePath: "/",
   reactStrictMode: true,
 };
 
